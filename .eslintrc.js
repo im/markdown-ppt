18d4cf91f5ec13154aab2c2524491343aff20fea
tangxiaomi <81195314@qq.com>
2022-12-06 09:30:52 +0800

    update

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    'root': true,
+    'extends': [
+        '@tangxiaomi'
+    ],
+    env: {
+        node: true,
+        'vue/setup-compiler-macros': true
+    },
+    'rules': {
+        'vue/valid-v-else': 'off',
+        'vue/html-indent': 'off',
+        'vue/require-default-prop': 'off',
+        'vue/first-attribute-linebreak': 'off',
+        'vue/max-attributes-per-line': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'vue/no-multiple-template-root': 'off',
+        '@typescript-eslint/indent': [
+            'error',
+            4
+        ]
+    }
+}
